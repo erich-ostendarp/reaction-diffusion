fff90bb054c46a985255fb4f27c361852422c87d
eostendarp <eostendarp@gmail.com>
2026-04-28 21:51:27 +0000

    write pixels to screen

--- src/root.zig
+++ src/root.zig
@@ -1,39 +1,53 @@
 const std = @import("std");
+const c = @import("c");
 const Io = std.Io;
 
-const c = @import("c");
+const Chemical = struct {
+    reaction_rate: f64,
+    diffusion_rate: f64,
+    amount: f64 = 0,
+    color: struct { u8, u8, u8 } = .{ 0, 255, 0 },
+};
+
+const Cell = struct {
+    producer: Chemical = .{ .reaction_rate = 0.1, .diffusion_rate = 0.1, .color = .{ 255, 0, 0 } },
+    consumer: Chemical = .{ .reaction_rate = 0.1, .diffusion_rate = 0.1, .color = .{ 0, 0, 255 } },
+};
 
 pub fn main(init: std.process.Init) !void {
     _ = init;
 
+    const window_width = 512;
+    const window_height = 512;
+
+    const board_width = 512;
+    const board_height = 512;
+
+    var cells: [board_width * board_height]Cell = undefined;
+    _ = &cells;
+
+    var pixels: [board_width * board_height * 3]u8 = [_]u8{ 0, 0, 0 } ** (board_width * board_height);
+    _ = &pixels;
+
+    _ = c.glfwSetErrorCallback(struct {
+        fn callback(a: c_int, b: [*c]const u8) callconv(.c) void {
+            std.debug.print("{} {s}\n", .{ a, b });
+        }
+    }.callback);
     if (c.glfwInit() != 1) return error.glfwInit;
 
-    const window = c.glfwCreateWindow(640, 480, "Hello, World!", null, null) orelse {
+    const window = c.glfwCreateWindow(window_width, window_height, "Hello, World!", null, null) orelse {
         c.glfwTerminate();
         return error.glfwCreateWindow;
     };
 
     c.glfwMakeContextCurrent(window);
     while (c.glfwWindowShouldClose(window) != 1) {
-        // Render here
-        c.glClear(c.GL_COLOR_BUFFER_BIT);
+        c.glRasterPos2i(-1, -1);
+        c.glDrawPixels(board_width, board_height, c.GL_RGB, c.GL_UNSIGNED_BYTE, @ptrCast(&pixels));
 
-        c.glColor3f(0, 0.5, 0.5);
-        c.glLineWidth(2);
-        c.glBegin(c.GL_LINE_LOOP);
-
-        c.glVertex2f(0, 0);
-        c.glVertex2f(0.5, 0);
-        c.glVertex2f(0.5, 0.5);
-        c.glVertex2f(0, 0.5);
-        c.glVertex2f(0, 0);
-
-        c.glEnd();
-
-        // Swap front and back buffers
         c.glfwSwapBuffers(window);
 
-        // Poll for and process events
         c.glfwPollEvents();
     }
 
